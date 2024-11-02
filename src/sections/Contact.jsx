0e780aab80668167497592b3e16f6b34380990ab
astkout <astkout12@hotmail.com>
2024-11-02 12:44:30 +0000

    cmt6

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,13 +1,13 @@
-import emailjs from '@emailjs/browser';
-import { useRef, useState } from 'react';
-import useAlert from '../hooks/useAlert.js';
-import Alert from '../components/Alert.jsx';
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
+import useAlert from "../hooks/useAlert.js";
+import Alert from "../components/Alert.jsx";
 
 const ContactForm = () => {
   const formRef = useRef();
   const { alert, showAlert, hideAlert } = useAlert();
   const [loading, setLoading] = useState(false);
-  const [form, setForm] = useState({ name: '', email: '', message: '' });
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
 
   const handleChange = ({ target: { name, value } }) => {
     setForm((prevForm) => ({ ...prevForm, [name]: value }));
@@ -31,7 +31,7 @@ const ContactForm = () => {
       from_email: form.email,
       message: form.message,
       reply_to: form.email,
-      to_email: 'astkou12@gmail.com', // Your email for receiving the message
+      to_email: "astkou12@gmail.com", // Your email for receiving the message
     };
 
     const emailDataToClient = {
@@ -40,16 +40,20 @@ const ContactForm = () => {
     };
 
     try {
-      // Send email to yourself
-      await sendEmail(import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID, emailDataToYou);
+      await sendEmail(
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        emailDataToYou
+      );
       console.log("Message to you sent successfully:", emailDataToYou);
 
-      // Send auto-reply to client
-      await sendEmail(import.meta.env.VITE_APP_EMAILJS_AUTO_REPLY_TEMPLATE_ID, emailDataToClient);
+      await sendEmail(
+        import.meta.env.VITE_APP_EMAILJS_AUTO_REPLY_TEMPLATE_ID,
+        emailDataToClient
+      );
       console.log("Auto-reply to client sent successfully:", emailDataToClient);
 
       showAlert("Message sent successfully!");
-      setForm({ name: '', email: '', message: '' }); // Clear the form after successful send
+      setForm({ name: "", email: "", message: "" });
     } catch (error) {
       console.error("Error sending email:", error);
       showAlert("Failed to send message. Please try again later.");
@@ -57,32 +61,64 @@ const ContactForm = () => {
       setLoading(false);
     }
   };
-    
 
   return (
     <section className="c-space my-0" id="contact">
       {alert.show && <Alert {...alert} />}
       <div className="relative min-h-screen flex items-center justify-center flex-col md:mt-20">
-        <img src="/assets/terminal.png" alt="terminal-bg" className="bg-image" />
+        <img
+          src="/assets/terminal.png"
+          alt="terminal-bg"
+          className="bg-image"
+        />
         <div className="contact-container md:mt-[100px] border border-black-200 rounded-lg p-5">
           <h3 className="head-text">Let's talk</h3>
           <p className="text-lg text-white-600 mt-3">
-            Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
-            life, I’m here to help.
+            Whether you’re looking to build a new website, improve your existing
+            platform, or bring a unique project to life, I’m here to help.
           </p>
 
-          <form ref={formRef} onSubmit={handleSubmit} className="mt-8 flex flex-col space-y-5">
+          <form
+            ref={formRef}
+            onSubmit={handleSubmit}
+            className="mt-8 flex flex-col space-y-5"
+          >
             <label className="space-y-3">
               <span className="field-label">Full Name</span>
-              <input type="text" name="name" value={form.name} onChange={handleChange} required className="field-input" placeholder="ex., John Doe" />
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="ex., John Doe"
+              />
             </label>
             <label className="space-y-3">
               <span className="field-label">Email address</span>
-              <input type="email" name="email" value={form.email} onChange={handleChange} required className="field-input" placeholder="ex., johndoe@gmail.com" />
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="ex., johndoe@gmail.com"
+              />
             </label>
             <label className="space-y-3">
               <span className="field-label">Your message</span>
-              <textarea name="message" value={form.message} onChange={handleChange} required rows={5} className="field-input" placeholder="Share your thoughts or inquiries..." style={{ resize: "none" }} />
+              <textarea
+                name="message"
+                value={form.message}
+                onChange={handleChange}
+                required
+                rows={5}
+                className="field-input"
+                placeholder="Share your thoughts or inquiries..."
+                style={{ resize: "none" }}
+              />
             </label>
             <button className="field-btn" type="submit" disabled={loading}>
               {loading ? (
@@ -92,7 +128,11 @@ const ContactForm = () => {
               ) : (
                 <>
                   Send Message
-                  <img src="/assets/arrow-up.png" alt="arrow-up" className="field-btn_arrow" />
+                  <img
+                    src="/assets/arrow-up.png"
+                    alt="arrow-up"
+                    className="field-btn_arrow"
+                  />
                 </>
               )}
             </button>
