a1c347b251d3c8980258f74b98158103dea0cbbf
astkout <astkout12@hotmail.com>
2024-12-16 10:39:48 +0000

    canvas loader

--- src/components/CanvasLoader.jsx
+++ src/components/CanvasLoader.jsx
@@ -1,37 +1,10 @@
-// import React from "react";
-// import { Html, useProgress } from "@react-three/drei";
-
-// const CanvasLoader = () => {
-//   const { progress } = useProgress();
-
-//   const displayText = progress !== 0 ? `${progress.toFixed(2)}%` : "Loading...";
-
-//   return (
-//     <Html
-//       as="div"
-//       center
-//       style={{
-//         display: "flex",
-//         justifyContent: "center",
-//         alignItems: "center",
-//         flexDirection: "column",
-//       }}
-//     >
-//       <span className="canvas-loader" />
-//       <p style={{ fontSize: 14, color: "#f1f1f1", fontWeight: 800 }}>
-//         {displayText}
-//       </p>
-//     </Html>
-//   );
-// };
-
-// export default CanvasLoader;
-
 import React from "react";
 import { Html, useProgress } from "@react-three/drei";
 
-const CanvasLoader = React.memo(() => {
+const CanvasLoader = () => {
   const { progress } = useProgress();
+
+  const displayText = progress !== 0 ? `${progress.toFixed(2)}%` : "Loading...";
 
   return (
     <Html
@@ -46,10 +19,10 @@ const CanvasLoader = React.memo(() => {
     >
       <span className="canvas-loader" />
       <p style={{ fontSize: 14, color: "#f1f1f1", fontWeight: 800 }}>
-        {progress.toFixed(2)}%
+        {displayText}
       </p>
     </Html>
   );
-});
+};
 
 export default CanvasLoader;
