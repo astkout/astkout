4dcf4ecca30bc489e0b38be17d6848c9a181b09d
astkout <astkout12@hotmail.com>
2024-12-16 09:58:24 +0000

    mesh changes

--- src/components/HackerRoom.jsx
+++ src/components/HackerRoom.jsx
@@ -7,97 +7,113 @@ export function HackerRoom(props) {
   const screenTexture = useTexture("textures/desk/screen.png");
 
   return (
-    <group {...props} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.computer_computer_mat_0.geometry}
-        material={materials.computer_mat}
-      >
-        <meshMatcapMaterial map={monitorTexture} />
-      </mesh>
+    // <group {...props} dispose={null}>
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.computer_computer_mat_0.geometry}
+    //     material={materials.computer_mat}
+    //   >
+    //     <meshMatcapMaterial map={monitorTexture} />
+    //   </mesh>
 
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.server_server_mat_0.geometry}
-        material={materials.server_mat}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.vhs_vhsPlayer_mat_0.geometry}
-        material={materials.vhsPlayer_mat}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.shelf_stand_mat_0.geometry}
-        material={materials.stand_mat}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.keyboard_mat_mat_mat_0.geometry}
-        material={materials.mat_mat}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.arm_arm_mat_0.geometry}
-        material={materials.arm_mat}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Tv_tv_mat_0.geometry}
-        material={materials.tv_mat}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.table_table_mat_0.geometry}
-        material={materials.table_mat}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Cables_cables_mat_0.geometry}
-        material={materials.cables_mat}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.props_props_mat_0.geometry}
-        material={materials.props_mat}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.screen_screens_0.geometry}
-        material={materials.screens}
-      >
-        <meshMatcapMaterial map={screenTexture} />
-      </mesh>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.screen_glass_glass_0.geometry}
-        material={materials.glass}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Ground_ground_mat_0.geometry}
-        material={materials.ground_mat}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.peripherals_key_mat_0.geometry}
-        material={materials.key_mat}
-      />
-    </group>
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.server_server_mat_0.geometry}
+    //     material={materials.server_mat}
+    //   />
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.vhs_vhsPlayer_mat_0.geometry}
+    //     material={materials.vhsPlayer_mat}
+    //   />
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.shelf_stand_mat_0.geometry}
+    //     material={materials.stand_mat}
+    //   />
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.keyboard_mat_mat_mat_0.geometry}
+    //     material={materials.mat_mat}
+    //   />
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.arm_arm_mat_0.geometry}
+    //     material={materials.arm_mat}
+    //   />
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.Tv_tv_mat_0.geometry}
+    //     material={materials.tv_mat}
+    //   />
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.table_table_mat_0.geometry}
+    //     material={materials.table_mat}
+    //   />
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.Cables_cables_mat_0.geometry}
+    //     material={materials.cables_mat}
+    //   />
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.props_props_mat_0.geometry}
+    //     material={materials.props_mat}
+    //   />
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.screen_screens_0.geometry}
+    //     material={materials.screens}
+    //   >
+    //     <meshMatcapMaterial map={screenTexture} />
+    //   </mesh>
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.screen_glass_glass_0.geometry}
+    //     material={materials.glass}
+    //   />
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.Ground_ground_mat_0.geometry}
+    //     material={materials.ground_mat}
+    //   />
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.peripherals_key_mat_0.geometry}
+    //     material={materials.key_mat}
+    //   />
+    // </group>
+    <group {...props} dispose={null}>
+    <mesh geometry={nodes.computer_computer_mat_0.geometry} material={materials.computer_mat} > <meshMatcapMaterial map={monitorTexture} /></mesh>
+    <mesh geometry={nodes.server_server_mat_0.geometry} material={materials.server_mat} />
+    <mesh geometry={nodes.vhs_vhsPlayer_mat_0.geometry} material={materials.vhsPlayer_mat} />
+    <mesh geometry={nodes.shelf_stand_mat_0.geometry} material={materials.stand_mat} />
+    <mesh geometry={nodes.keyboard_mat_mat_mat_0.geometry} material={materials.mat_mat} />
+    <mesh geometry={nodes.arm_arm_mat_0.geometry} material={materials.arm_mat} />
+    <mesh geometry={nodes.Tv_tv_mat_0.geometry} material={materials.tv_mat} />
+    <mesh geometry={nodes.table_table_mat_0.geometry} material={materials.table_mat} />
+    <mesh geometry={nodes.Cables_cables_mat_0.geometry} material={materials.cables_mat} />
+    <mesh geometry={nodes.props_props_mat_0.geometry} material={materials.props_mat} />
+    <mesh geometry={nodes.screen_screens_0.geometry} material={materials.screens} > <meshMatcapMaterial map={screenTexture}/></mesh>
+    <mesh geometry={nodes.screen_glass_glass_0.geometry} material={materials.glass} />
+    <mesh geometry={nodes.Ground_ground_mat_0.geometry} material={materials.ground_mat} />
+    <mesh geometry={nodes.peripherals_key_mat_0.geometry} material={materials.key_mat} />
+  </group>
   );
 }
 
